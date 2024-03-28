a4b1d959566ec2deaf926aea759f8747d447d9ed
oluwabajio <ismailosunlana@yahoo.com>
2024-03-28 19:03:09 +0100

    new

--- app/src/main/java/rotate/pdf/pages/MainActivity.kt
+++ app/src/main/java/rotate/pdf/pages/MainActivity.kt
@@ -20,6 +20,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        getWindow().getDecorView(); //to test app
         super.onCreate(savedInstanceState)
 
         binding = ActivityMainBinding.inflate(layoutInflater)
