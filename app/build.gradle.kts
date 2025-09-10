0fbe9ae475a08cacc352662ac279568960c54de4
oluwabajio <ismailosunlana@yahoo.com>
2025-09-10 01:43:19 +0100

    new o

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,14 @@ plugins {
 
 android {
     namespace = "rotate.pdf.pages"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "rotate.pdf.pages"
         minSdk = 24
-        targetSdk = 34
-        versionCode = 2
-        versionName = "1.2"
+        targetSdk = 36
+        versionCode = 5
+        versionName = "1.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
@@ -56,15 +56,13 @@ android {
 
     dependencies {
 
-        implementation("androidx.core:core-ktx:1.12.0")
-        implementation("androidx.appcompat:appcompat:1.6.1")
-        implementation("com.google.android.material:material:1.11.0")
-        implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-        implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
-        implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-        implementation("com.google.android.gms:play-services-ads-lite:23.0.0")
-        testImplementation("junit:junit:4.13.2")
-        androidTestImplementation("androidx.test.ext:junit:1.1.5")
-        androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-        implementation("com.itextpdf:itext7-core:8.0.0")
+        implementation("androidx.core:core-ktx:1.17.0")
+        implementation("androidx.appcompat:appcompat:1.7.1")
+        implementation("com.google.android.material:material:1.13.0")
+        implementation("androidx.constraintlayout:constraintlayout:2.2.1")
+        implementation("androidx.navigation:navigation-fragment-ktx:2.9.3")
+        implementation("androidx.navigation:navigation-ui-ktx:2.9.3")
+        implementation("com.google.android.gms:play-services-ads-lite:24.3.0")
+
+        implementation("com.itextpdf:itext7-core:9.3.0")
     }
